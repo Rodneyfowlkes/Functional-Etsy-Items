e2470a06e1d9e28ceba889833a45ef3fbf25b970
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-06-18 00:13:08 -0400

    Completed all but the last function

--- main.js
+++ main.js
@@ -44,3 +44,51 @@ return gbp_item;
 
 
 document.querySelector("#answer3").textContent = goat(items)
+
+
+
+
+var hen = function (x) {
+  var wood_items = []
+  x.forEach(function(n) {
+  if (n.materials.includes("wood")){ 
+    wood_items.push(n.title);
+  }
+
+ });
+return wood_items;
+};
+
+document.querySelector("#answer4").textContent = hen(items)
+
+
+var fox = function (x) {
+  var eight_items = []
+  x.forEach(function(n) {
+  if (n.materials.length >= 8){ 
+    eight_items.push(n.quantity + n.title + n.materials);
+  }
+
+ });
+return eight_items;
+};
+
+document.querySelector("#answer5").textContent = fox(items)
+
+var alpaca = function (x) {
+  var self_made_items = []
+  x.forEach(function(n) {
+  if (n.who_made === "i_did"){ 
+    self_made_items.push(n);
+  }
+
+ });
+return self_made_items.length;
+};
+
+
+
+
+
+
+
